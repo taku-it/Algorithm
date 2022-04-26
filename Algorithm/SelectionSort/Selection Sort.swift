4403eff6950be1292d0ef7c2f1c7a3e4d9e756c9
Takuto Ikuta <58894683+taku-it@users.noreply.github.com>
2022-04-27 01:38:47 +0900

    選択ソート

---
+++ Algorithm/SelectionSort/Selection Sort.swift
@@ -0,0 +1,31 @@
+//
+//  Selection Sort.swift
+//  coding
+//
+//  Created by 生田拓登 on 2022/04/27.
+//
+
+import Foundation
+
+var array = [3,1,55,2,28,19,31,93,6]
+
+func selectionSort() {
+    for i in 0..<array.count {
+        var min_value = array[i]
+        var min_index = i
+    //    最小の要素を確認する
+        for j in i..<array.count{
+            if array[j] < min_value{
+                min_value = array[j]
+                min_index = j
+            }
+            
+        }
+    //    要素の入れ替え
+        let tmp = array[i]
+        array[i] = array[min_index]
+        array[min_index] = tmp
+    }
+
+    print(array)
+}
